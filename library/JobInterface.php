f9931406e940ba34bfdd770b6b08095200532575
Chris Sandoval <chrissandoval916@yahoo.com>
2019-04-26 11:55:30 -0700

    Add default val for param

--- library/JobInterface.php
+++ library/JobInterface.php
@@ -4,7 +4,7 @@ namespace ryancco\forker;
 
 interface JobInterface
 {
-    public function __invoke($instanceNumber);
+    public function __invoke($instanceNumber = 1);
 
     public function __destruct();
 }
